8b1e88649aff44863c28f72383c19cc290066323
ecbDeveloper <developer.ecb@gmail.com>
2025-08-15 19:00:09 -0300

    adiciona csrf na aplicação

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,22 +1,25 @@
 package api
 
 import (
+	"os"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gorilla/csrf"
 )
 
 func (api *Api) BindRoutes() {
 	api.Router.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger, api.Sessions.LoadAndSave)
 
-	// csrfMiddleware := csrf.Protect(
-	// 	[]byte(os.Getenv("GOBID_CSRF_KEY")),
-	// 	csrf.Secure(false), // DEV ONLY
-	// )
+	csrfMiddleware := csrf.Protect(
+		[]byte(os.Getenv("GOBID_CSRF_KEY")),
+		csrf.Secure(false), // DEV ONLY
+	)
 
-	// api.Router.Use(csrfMiddleware)
+	api.Router.Use(csrfMiddleware)
 
 	api.Router.Route("/api", func(r chi.Router) {
-		// r.Get("/csrftoken", api.HandleGetCSRFtoken)
+		r.Get("/csrftoken", api.HandleGetCSRFtoken)
 		api.bindUsersRoutes(r)
 		api.bindProductsRoutes(r)
 		api.bindBidsRoutes(r)
